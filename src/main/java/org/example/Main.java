d15229d568190f34f5288ea28de06ce82e59180b
Iurii <yurasik96@inbox.ru>
2024-04-20 16:46:40 +0500

    Initial

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+//        Calculator calc = new Calculator();
+//        System.out.println(
+//                calc.newFormula()
+//                        .addOperand(5)
+//                        .addOperand(15)
+//                        .calculate(Calculator.Operation.MULT)
+//                        .result()
+//        );
+
+        Ints intsCalc = new IntsCalculator();
+        System.out.println(intsCalc.sum(2, 2));
+        System.out.println(intsCalc.sum(10, 22));
+        System.out.println(intsCalc.pow(2, 10));
+    }
+}
